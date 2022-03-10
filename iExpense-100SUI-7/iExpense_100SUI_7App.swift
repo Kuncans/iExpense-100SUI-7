c450debdf3632db64ab21a25cb6e896ba075d7be
Kuncans <akafauna@gmail.com>
2022-03-10 12:05:36 +0000

    Initial Commit

---
+++ iExpense-100SUI-7/iExpense_100SUI_7App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_100SUI_7App.swift
+//  iExpense-100SUI-7
+//
+//  Created by Duncan Kent on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_100SUI_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
